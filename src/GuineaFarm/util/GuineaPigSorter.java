cfb60118d23c9e4e64b32308515bec6aa812ae99
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-03-09 19:07:08 +0100

    kk

--- src/GuineaFarm/util/GuineaPigSorter.java
+++ src/GuineaFarm/util/GuineaPigSorter.java
@@ -1,8 +1,8 @@
 package GuineaFarm.util;
 
 import GuineaFarm.model.GuineaPig;
-
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /*
  Utility class for sorting guinea pigs.
@@ -13,22 +13,17 @@ import java.util.ArrayList;
 
 public class GuineaPigSorter {
 
-    public static void sortByName(ArrayList<GuineaPig> pigs) {
-
-        // STUDENT TASK
-
+    public static void sortByName(ArrayList<GuineaPig> pigs ) {
+        pigs.sort(Comparator.comparing(GuineaPig::getName));// STUDENT TASK
     }
 
     public static void sortByAge(ArrayList<GuineaPig> pigs) {
-
-        // STUDENT TASK
+        pigs.sort(Comparator.comparingInt(GuineaPig::getAge));
 
     }
 
     public static void sortByWeight(ArrayList<GuineaPig> pigs) {
-
-        // STUDENT TASK
+        pigs.sort(Comparator.comparingDouble(GuineaPig::getWeight));
 
     }
-
 }
